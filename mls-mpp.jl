e658aac2b45bafab8a1ea77928223418287364de
Jeremy Dudo <jmdudo2@gmail.com>
2019-07-21 15:08:00 -0400

    updating mappings and linalg into native julia

--- mls-mpp.jl
+++ mls-mpp.jl
@@ -18,9 +18,9 @@ const ν = 0.2;
 const μ₀ = E / (2 * (1 + ν));
 const λ₀ = E * ν / ((1+ν) * (1 - 2 * ν));
 
-v = [0,0];       # velocity
-F = [1,0, 0,1];  # Deformation tensor
-C = [0,0, 0,0];  # Cauchy tensor
+v = [0, 0];       # velocity
+F = [1, 0, 0, 1];  # Deformation tensor
+C = [0, 0, 0, 0];  # Cauchy tensor
 Jp = 1;          # Jacobian determinant (scalar)
 
 mutable struct Particle
@@ -49,33 +49,33 @@ function advance(dt)
 
         # Quadratic kernels  [http:#mpm.graphics   Eqn. 123, with x=fx, fx-1,fx-2]
         const w = [
-            had2D([0.5, 0.5], sub2D([1.5, 1.5], fx).map(o=>o*o)),
-            sub2D([0.75, 0.75], sub2D(fx, [1.0, 1.0]).map(o=>o*o)),
-            had2D([0.5, 0.5], sub2D(fx, [0.5, 0.5]).map(o=>o*o))
+            map(x -> x^2, (0.5 .* (1.5 .- fx))),
+            map(x -> x^2, (0.75 .- (fx .- 1.0))),
+            map(x -> x^2, (0.5 .* (fx .- 0.5)))
         ];
 
         # Snow-like hardening
-        const e = Math.exp(hardening * (1.0 - p.Jp));
-        const mu=mu_0*e;
-        const lambda=lambda_0*e;
+        const e = exp(hardening * (1.0 - p.Jp));
+        const μ = μ₀ * e;
+        const λ = λ₀ * e;
 
         # Cauchy stress times dt and inv_dx
         # original taichi: stress = -4*inv_dx*inv_dx*dt*vol*( 2*mu*(p.F-r)*transposed(p.F) + lambda*(J-1)*J )
         # (in taichi matrices are coded transposed)
-        const J = determinant(p.F); # Current volume
+        const J = det(p.F); # Current volume
         const {R:r, S:s} = polar_decomp(p.F); # Polar decomp. for fixed corotated model
         const k1 = -4*inv_dx*inv_dx*dt*vol;
-        const k2 = lambda*(J-1)*J;
-        const stress = addMat( mulMat(subMat(transposed(p.F),r),p.F).map(o=>o*2*mu), [k2,0,0,k2] ).map(o=>o*k1);
-        const affine = addMat(stress, p.C.map(o=>o*particle_mass));
+        const k2 = λ*(J-1)*J;
+        const stress = map(x -> x*k1, ( map(x-> x*2*μ, mulMat((p.F'.- r),p.F)) .+ [k2,0,0,k2] ) );
+        const affine = stress .+ map(x -> x*particle_mass, p.C);
 
-        const mv = [p.v[0]*particle_mass, p.v[1]*particle_mass, particle_mass]; # translational momentum
+        const mv = [p.v[1]*particle_mass, p.v[2]*particle_mass, particle_mass]; # translational momentum
         for i = 1:3, j = 1:3
             # scatter to grid
-            const dpos = [(i-fx[0])*dx, (j-fx[1])*dx];
-            const ii = gridIndex(base_coord[0] + i, base_coord[1] + j);
-            const weight = w[i][0] * w[j][1];
-            grid[ii] = add3D(grid[ii], sca3D(add3D(mv, [...mulMatVec(affine, dpos),0]), weight));
+            const dpos = [(i-fx[1])*dx, (j-fx[2])*dx];
+            const ii = gridIndex(base_coord[1] + i, base_coord[2] + j);
+            const weight = w[i][1] * w[j][2];
+            grid[ii] = (grid[ii] .+ ((mv .+ [mulMatVec(affine, dpos)[1], mulMatVec(affine, dpos)[2],0]) .* weight));
         end
     end
 
@@ -85,8 +85,8 @@ function advance(dt)
         # for all grid nodes
         const ii = gridIndex(i, j);
         if (grid[ii][2] > 0)  # no need for epsilon here
-            grid[ii] = grid[ii].map(o=>o/grid[ii][2]); # normalize by mass
-            grid[ii] = add3D(grid[ii], [0,-200*dt,0]); # add gravity
+            grid[ii] = map(x -> x/grid[ii][3], grid[ii]); # normalize by mass
+            grid[ii] = grid[ii] .+ [0, -200*dt, 0]; # add gravity
             const x = i/n;
             const y = j/n; # boundary thickness, node coord
 
@@ -97,15 +97,15 @@ function advance(dt)
 
             # separate
             if (y < boundary) 
-                grid[ii][1] = Math.max(0.0, grid[ii][1]);
+                grid[ii][2] = max(0.0, grid[ii][2]);
             end
         end
     end
 
     # 2. Grid to particle
     for p in particles
-        const base_coord=sub2D(p.x.map(o=>o*inv_dx),[0.5,0.5]).map(o=>parseInt(o));# element-wise floor
-        const fx = sub2D(sca2D(p.x, inv_dx), base_coord); # base position in grid units
+        const base_coord=map(x -> Int(round(x)), (map(x -> x*inv_dx, p.x) .- 0.5));# element-wise floor
+        const fx = ((p.x .* inv_dx) .- base_coord); # base position in grid units
         const w = [
             had2D([0.5, 0.5], sub2D([1.5, 1.5], fx).map(o=>o*o)),
             sub2D([0.75, 0.75], sub2D(fx, [1.0, 1.0]).map(o=>o*o)),
