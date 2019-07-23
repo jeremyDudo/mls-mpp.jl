0293075e74b4599fa217735f09303b9c478c544d
Jeremy Dudo <jmdudo2@gmail.com>
2019-07-22 23:44:23 -0400

    add_rnd_square2 returns a list of objects... hopefully can have an updated advance!() function to take each square separately? I don't know if this is the best idea, but it would help separating the files...

--- mls-mpp.jl
+++ mls-mpp.jl
@@ -153,3 +153,12 @@ function add_rnd_square(center, c)
         push!(particles, (Particle(([(rand()*2-1)*0.08, (rand()*2-1)*0.08] .+ center), v, F, C, Jp, c)));
     end
 end
+
+function add_rnd_square2(center, c)
+    party = []
+    for i = 1:1000 
+        # Randomly sample 1000 particles in the square
+        push!(party, (Particle(([(rand()*2-1)*0.08, (rand()*2-1)*0.08] .+ center), v, F, C, Jp, c)));
+    end
+    party
+end
