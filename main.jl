b63b589d7344bb734d0909030ecf3c11f11dab56
Jeremy Dudo <jmdudo2@gmail.com>
2019-07-22 23:36:52 -0400

    beginning plotting setup

--- main.jl
+++ main.jl
@@ -3,14 +3,19 @@ using Plots
 
 
 function display()
-
+    plot = 0;
 end
 
-function step()
-
+function step!()
+    advance()
 end
 
 function main()
+
+    # setup 
+    square1 = add_rnd_square([0.55,0.45], 0xED553B);
+    square2 = add_rnd_square([0.45,0.65], 0xF2B134);
+    square3 = add_rnd_square([0.55,0.85], 0x168587);
 
 
 end
