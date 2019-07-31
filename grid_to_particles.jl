26c5f869e7b63be0c677c9bf730d73f9059d1a84
Jeremy Dudo <jmdudo2@gmail.com>
2019-07-30 23:57:26 -0400

    found where the bug is -- grid has wrong dimenstions for the casting in the loop

--- grid_to_particles.jl
+++ grid_to_particles.jl
@@ -4,7 +4,7 @@ include("mulMat.jl")
 
 using LinearAlgebra;
 
-function grid_to_particles!(party, n, grid, inv_dx, plastic)
+function grid_to_particles!(party, n, grid, inv_dx, plastic, dt)
     for p in party
         base_coord= floor.( Int, (inv_dx .* p.x .- 0.5));# element-wise floor
         fx = ((p.x .* inv_dx) .- base_coord); # base position in grid units
@@ -13,13 +13,18 @@ function grid_to_particles!(party, n, grid, inv_dx, plastic)
             (0.75 .- map(x -> x^2, (fx .- 1.0))),
             (0.5 .* map(x -> x^2, (fx .- 0.5)))
         ];
-        p.C = [0,0, 0,0];
+        p.C = [0 0; 0 0];
         p.v = [0, 0];
         for i = 1:3, j = 1:3 
             dpos =    [i, j] .- fx;
             ii =      gridIndex(base_coord[1] + i, base_coord[2] + j, n);
             weight =  w[i][1] * w[j][2];
+            println(p.C)
+            println(grid[ii])
+            println(weight)
+            println(outer_product((grid[ii] .* weight), dpos))
             p.v =     (p.v .+ (grid[ii] .* weight)); # velocity
+
             p.C =     map(x -> x*4*inv_dx, p.C .+ outer_product((grid[ii] .* weight), dpos)); # APIC (affine particle-in-cell); p.C is the affine momentum
         end
 
@@ -28,7 +33,7 @@ function grid_to_particles!(party, n, grid, inv_dx, plastic)
 
         # MLS-MPM F-update
         # original taichi: F = (Mat(1) + dt * p.C) * p.F
-        F = mulMat(p.F, ([1,0, 0,1] .+ dt .* p.C));
+        F = mulMat(p.F, ([1 0; 0 1] .+ dt .* p.C));
 
         # Snow-like plasticity
         M = svd(F);
